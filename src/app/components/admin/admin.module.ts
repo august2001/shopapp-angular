ec5c5d8193e2f12ac6f8b31d36cb48eaed83e544
Dat <ntdat200801@gmail.com>
2024-05-04 20:40:49 +0700

    update features

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -6,7 +6,13 @@ import { AdminComponent } from './admin.component';
 import { OrderAdminComponent } from './order/order.admin.component';
 import { DetailOrderAdminComponent } from './detail-order/detail.order.admin.component';
 import { ProductAdminComponent } from './product/product.admin.component';
+import { UpdateProductAdminComponent } from './product/update/update.product.admin.component';
+import { InsertProductAdminComponent } from './product/insert/insert.product.admin.component';
+
 import { CategoryAdminComponent } from './category/category.admin.component';
+import { InsertCategoryAdminComponent } from './category/insert/insert.category.admin.component';
+import { UpdateCategoryAdminComponent } from './category/update/update.category.admin.component';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -14,10 +20,15 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AdminComponent,
     OrderAdminComponent,
-    DetailOrderAdminComponent,
-
     ProductAdminComponent,
     CategoryAdminComponent,
+    //sub-components
+    DetailOrderAdminComponent,
+    UpdateProductAdminComponent,
+    InsertProductAdminComponent,
+
+    InsertCategoryAdminComponent,
+    UpdateCategoryAdminComponent
   ],
   imports: [
     AdminRoutingModule, // import routes,
